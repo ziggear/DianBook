a2e7430e367024df7fab93cf37e159f018e0a515
ziggear <ziggear@gmail.com>
2012-07-27 14:28:00 +0800

    some changes

---
+++ Pages/Loading.h
@@ -0,0 +1,13 @@
+//
+//  Loading.h
+//  DianBook
+//
+//  Created by 1 1 on 7/27/12.
+//  Copyright (c) 2012 111. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Loading : NSObject
+
+@end
---
+++ Pages/Loading.m
@@ -0,0 +1,13 @@
+//
+//  Loading.m
+//  DianBook
+//
+//  Created by 1 1 on 7/27/12.
+//  Copyright (c) 2012 111. All rights reserved.
+//
+
+#import "Loading.h"
+
+@implementation Loading
+
+@end
